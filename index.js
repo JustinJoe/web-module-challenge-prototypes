af478a37c37e5ee36b8bc609650886833bdb6de0
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-05-28 17:09:03 +0000

    Completes Task 3

--- index.js
+++ index.js
@@ -69,11 +69,17 @@ Car.prototype.fill = function(gallons) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+// Baby.prototype.constructor = Baby.prototype;
 
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
